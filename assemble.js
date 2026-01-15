b034e3463aba69572cc9e37ebd7a03c32f4e68d5
Cesium72 <124750737+Cesium72@users.noreply.github.com>
2026-01-14 20:23:06 -0700

    Added prime find program (part 2)

--- assemble.js
+++ assemble.js
@@ -6,7 +6,7 @@ with it. However, you might want to make
 some improvements - this was rushed.
 */
 
-const examples = ["run_onload", "list_chars", "guess_game", "greet_me", "rck_pr_srs"];
+const examples = ["run_onload", "list_chars", "guess_game", "greet_me", "rck_pr_srs", "prime_find"];
 
 const table = {
   "nop":10,
